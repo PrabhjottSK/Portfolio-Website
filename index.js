14cdf8741f4d95d40d96eca9dc2f832a2751f37a
Prabhjott Singh Kusrath <prabhjottkusrath@Prabhjotts-Air.fios-router.home>
2022-07-23 21:32:45 -0400

    Intial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// Create back-end for contact page
+// Special option for 'requesting phone number' which would add automated msg to DB.
+// Special option for 'requesting resume' which would add automated msg to DB.
+// 'email here' has requested access to your resume.
